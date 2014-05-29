89aa3219229b0debc065765411e6f649e1822991
Conn O'Griofa <connogriofa@gmail.com>
2014-05-29 08:37:38 +0100

    Bringup for cm-11.0
    * Remove obsolete files
    * Update MMS config to use proper profile for S5830i
    * Switch from LDPI to MDPI; enable mdpi overlay.
    * Reformat BoardConfig
    * Fix build errors
    
    Change-Id: I8321834d36d5cde1407d0898c404d54cd73ee346

--- full_cooperve.mk
+++ full_cooperve.mk
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,21 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_small.mk)
-
-# This is where we'd set a backup provider if we had one
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
-
 # Inherit device specific files
-$(call inherit-product, device/samsung/cooperve/device.mk)
+$(call inherit-product, device/samsung/cooperve/device_cooperve.mk)
 
 # Overrides
 PRODUCT_NAME := full_cooperve
 PRODUCT_DEVICE := cooperve
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-S5830i
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_CHARACTERISTICS := phone
